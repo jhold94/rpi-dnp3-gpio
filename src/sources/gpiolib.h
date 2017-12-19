35c2a7505a091259e0c08b303dbff707b04f5a7e
jhold94 <33321625+jhold94@users.noreply.github.com>
2017-12-19 15:38:11 -0500

    Update gpiolib.h

--- src/sources/gpiolib.h
+++ src/sources/gpiolib.h
@@ -3,15 +3,16 @@
 extern "C" {
 #endif
 
-extern    int gpio_direction(int gpio, int dir);
-extern    int gpio_export(int gpio);
-extern    void gpio_unexport(int gpio);
-extern    int gpio_read(int gpio);
-extern    int gpio_write(int gpio, int val);
-extern    void pinMode(int pin, int mode);
-extern    void digitalWrite(int pin, int state);
-extern    int digitalRead(int pin);
-extern    int analogRead(int pin);
+  extern    int gpio_direction(int gpio, int dir);
+  extern    int gpio_export(int gpio);
+  extern    void gpio_unexport(int gpio);
+  extern    int gpio_read(int gpio);
+  extern    int gpio_write(int gpio, int val);
+  extern    void pinMode(int pin, int mode);
+  extern    void digitalWrite(int pin, int state);
+  extern    int digitalRead(int pin);
+  extern    void analogPinMode(int pin);
+  extern    int analogRead(int pin);
   
 #ifdef __cplusplus
 }
