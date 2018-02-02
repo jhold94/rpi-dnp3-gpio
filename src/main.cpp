7fa2ea6026ef326204d831775ec34dc0f9ed693f
jhold94 <33321625+jhold94@users.noreply.github.com>
2018-02-02 15:11:51 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -16,7 +16,7 @@
 #include "Config.h"
 #include "GPIOCommandHandler.h"
 #include "sources/gpiolib.h"
-#include "sources/modbusCommand.h"
+#include "sources/modbusCommands.h"
 
 // prototypes for handling ini file reading
 int cfg_handler(void* user, const char* section, const char* name, const char* value);
