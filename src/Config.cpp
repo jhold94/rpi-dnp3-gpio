4d7e39338610adeb82eac4da1e29a36935dd7002
jhold94 <33321625+jhold94@users.noreply.github.com>
2018-02-13 17:37:32 -0500

    Update Config.cpp

--- src/Config.cpp
+++ src/Config.cpp
@@ -35,7 +35,7 @@ bool Config::Add(uint8_t gpiopin, assignve0c& vec0)
     return true;
 }
 
-bool Config::Add(uint8_t analogpin, assignvec1& vec1)
+bool Config::Add1(uint8_t analogpin, assignvec1& vec1)
 {
     // first check if the pin is assigned
     if(assigned.find(analogpin) != assigned.end())
