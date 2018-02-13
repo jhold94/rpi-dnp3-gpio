3f14504531e1b9aef141899e3dd340d1c6b3ea8f
jhold94 <33321625+jhold94@users.noreply.github.com>
2018-02-13 14:09:16 -0500

    Update GPIOCommandHandler.cpp

--- src/GPIOCommandHandler.cpp
+++ src/GPIOCommandHandler.cpp
@@ -42,6 +42,34 @@ CommandStatus GPIOCommandHandler::Operate(const ControlRelayOutputBlock& command
     return ret;
 }
 
+opendnp3::CommandStatus Select(const opendnp3::AnalogOutputInt16& command, uint16_t index)
+{
+    uint8_t analogpin = 0;
+    uint16_t value = 0;
+    
+    return GetPinAndValue(index, command.functoinCode, analogpin, value);
+}
+
+opendnp3::CommandStatus Operate(const opendnp3::AnalogOutputInt16& command, uint16_t index, opendnp3::OperateType opType)
+{
+    uint8_t analogpin = 0;
+    uint16_t value = 0;
+    
+    auto ret = GetPinAndValue(index, command.functionCode, analogpin, value);
+    
+    if(ret == CommandStatus::SUCCESS)
+    {
+        if(gpio < 10)
+        {
+            analogWrite(analogpin, value);
+        } else {
+            dmWriteRegs((analogpin  - 10), value);
+        }
+    }
+    
+    return ret;
+}
+
 CommandStatus GPIOCommandHandler::GetPinAndState(uint16_t index, opendnp3::ControlCode code, uint8_t& gpio, bool& state)
 {
     switch(code)
@@ -63,4 +91,9 @@ CommandStatus GPIOCommandHandler::GetPinAndState(uint16_t index, opendnp3::Contr
 
     gpio = iter->second;
     return CommandStatus::SUCCESS;
+}
+
+CommandStatus GPIOCommandHandler::GetPinAndValue(uint16_t index, opendnp3::ControlCode code, uint8_t& analogpin, uint16_t& value)
+{
+    //do stuff here
 }
