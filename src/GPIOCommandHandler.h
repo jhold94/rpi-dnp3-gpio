d886ea4476f6fa1247d9f3ae5e6cc77981f087ba
jhold94 <33321625+jhold94@users.noreply.github.com>
2018-02-13 14:32:00 -0500

    Update GPIOCommandHandler.h

--- src/GPIOCommandHandler.h
+++ src/GPIOCommandHandler.h
@@ -19,11 +19,9 @@ public:
 
     opendnp3::CommandStatus Operate(const opendnp3::ControlRelayOutputBlock& command, uint16_t index, opendnp3::OperateType opType) override;
 
-    opendnp3::CommandStatus Select(const opendnp3::AnalogOutputInt16& command, uint16_t index) override
-    { return opendnp3::CommandStatus::NOT_SUPPORTED; }
+    opendnp3::CommandStatus Select(const opendnp3::AnalogOutputInt16& command, uint16_t index) override;
 
-    opendnp3::CommandStatus Operate(const opendnp3::AnalogOutputInt16& command, uint16_t index, opendnp3::OperateType opType) override
-    { return opendnp3::CommandStatus::NOT_SUPPORTED; }
+    opendnp3::CommandStatus Operate(const opendnp3::AnalogOutputInt16& command, uint16_t index, opendnp3::OperateType opType) override;
 
     opendnp3::CommandStatus Select(const opendnp3::AnalogOutputInt32& command, uint16_t index) override
     { return opendnp3::CommandStatus::NOT_SUPPORTED; }
@@ -48,6 +46,10 @@ private:
     opendnp3::CommandStatus GetPinAndState(uint16_t index, opendnp3::ControlCode code, uint8_t& gpio, bool& state);
 
     std::map<uint16_t, uint8_t> dnp2gpio;
+    
+    opendnp3::CommandStatus GetPinAndValue(uint16_t index, opendnp3::ControlCode code, uint8_t& analogpin, uint16_t& value);
+    
+    std::map<uint16_t, uint8_t> dnp2analogpin;
 };
 
 #endif //TS_DNP3_GPIOCOMMANDHANDLER_H
