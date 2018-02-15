8cbbc837285f600e27941a1ade4d31ec64fc1167
jhold94 <33321625+jhold94@users.noreply.github.com>
2018-02-15 12:10:36 -0500

    Update modbusCommands.cpp

--- src/modbusCommands.cpp
+++ src/modbusCommands.cpp
@@ -16,7 +16,7 @@ void modbus_init(void)
 	int s;
         mb = modbus_new_tcp("192.168.24.24", 502);
         modbus_connect(mb);
-	modbus_tcp_listen(mb, 1);
+	s = modbus_tcp_listen(mb, 1);
 	modbus_tcp_accept(mb, &s);
 }
 
