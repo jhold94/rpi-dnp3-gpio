02a1f0ae597bd4afa092b89ad041f33680f9a58b
jhold94 <33321625+jhold94@users.noreply.github.com>
2018-02-20 17:37:19 -0500

    Update modbusCommandHandler.cpp

--- src/modbusCommandHandler.cpp
+++ src/modbusCommandHandler.cpp
@@ -85,12 +85,9 @@ void dmWriteBit(int index, bool state)
         {
                 tab_bit[index] = 1;
                 modbus_write_bit(ctx, index, ON);
-        } else if (state == FALSE)
-        {
+        } else {
                 tab_bit[index] = 0;
                 modbus_write_bit(ctx, index, OFF);
-        } else {
-                std::cout << "State Error in modbusCommandHandler.cpp" << std::endl;
         }
 }
 
