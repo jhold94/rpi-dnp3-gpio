aaa25884b3a87875dae46fe47be34a37d8705606
jhold94 <33321625+jhold94@users.noreply.github.com>
2018-02-21 10:44:13 -0500

    Update Config.cpp

--- src/Config.cpp
+++ src/Config.cpp
@@ -1,27 +1,27 @@
 
 #include "Config.h"
 
-bool Config::AddInput(uint8_t gpiopin)
+bool Config::AddInput(uint16_t gpiopin)
 {
     return Add(gpiopin, inputs);
 }
 
-bool Config::AddAninput(uint8_t gpiopin)
+bool Config::AddAninput(uint16_t gpiopin)
 {
     return Add(gpiopin, aninputs);
 }
 
-bool Config::AddOutput(uint8_t gpiopin)
+bool Config::AddOutput(uint16_t gpiopin)
 {
     return Add(gpiopin, outputs);
 }
 
-bool Config::AddAnoutput(uint8_t gpiopin)
+bool Config::AddAnoutput(uint16_t gpiopin)
 {
     return Add(gpiopin, anoutputs);
 }
 
-bool Config::Add(uint8_t gpiopin, assignvec0& vec0)
+bool Config::Add(uint16_t gpiopin, assignvec0& vec0)
 {
     // first check if the pin is assigned
     if(assigned.find(gpiopin) != assigned.end())
