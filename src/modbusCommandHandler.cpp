b5279e8a7ff4f6ce20b53bfebe1437d7abfa9abf
jhold94 <33321625+jhold94@users.noreply.github.com>
2018-02-22 17:14:22 -0500

    Update modbusCommandHandler.cpp

--- src/modbusCommandHandler.cpp
+++ src/modbusCommandHandler.cpp
@@ -16,6 +16,7 @@
 
 uint8_t *tab_bit;
 uint8_t *tab_input_bit;
+uint16_t *tab_input_reg;
 uint16_t *tab_reg;
 modbus_t *ctx;
 int nb_points;
@@ -41,6 +42,9 @@ void modbus_init(void)
         /* Allocate and initialize the memory to store Input Bits */
         tab_input_bit = (uint8_t *) malloc(MAX_READ_BITS * sizeof(uint8_t));
         memset(tab_input_bit, 0, MAX_READ_BITS * sizeof(uint8_t));
+        
+        tab_input_reg = (uint16_t *) malloc(MAX_READ_REGISTERS * sizeof(uint16_t));
+        memset(tab_input_reg, 0, MAX_READ_REGISTERS * sizeof(uint16_t));
         
         tab_reg = (uint16_t *) malloc(MAX_READ_REGISTERS * sizeof(uint16_t));
         memset(tab_reg, 0, MAX_READ_REGISTERS * sizeof(uint16_t));
@@ -70,9 +74,9 @@ void dmWriteBit(int index, bool state)
 {
         index = index - 1000;
         
-        tab_bit[index] = state;
+        //tab_bit[index] = state;
         
-        modbus_write_bit(ctx, index, tab_bit[index]);
+        modbus_write_bit(ctx, index, state); //tab_bit[index]);
 }
 
 long dmReadReg(int index)
@@ -80,9 +84,26 @@ long dmReadReg(int index)
         index = index - 30000;
         
         nb_points_reg = MAX_READ_REGISTERS;
-        modbus_read_registers(ctx, 0, nb_points_reg, tab_reg);
+        modbus_read_input_registers(ctx, 0, nb_points_reg, tab_input_reg);
                 
         return tab_reg[index];        
+}
+
+long dmReadHoldReg(int index)
+{
+        index = index - 40000;
+        
+        nb_points_reg = MAX_READ_REGISTERS;
+        modbus_read_registers(ctx, 0, nb_points_reg, tab_reg);
+}
+
+void dmWriteReg(int index, long value)
+{
+        index = index - 40000;
+        
+        //tab_reg[index] = value;
+        
+        modbus_write_registers(ctx, index, value); //tab_reg[index]);       
 }
 
 void modbus_exit(void)
